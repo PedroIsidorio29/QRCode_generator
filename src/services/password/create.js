f77063ee8f45b133ea3a4510278d06b4aeb646dc
Pedro Henrique Isidorio de Souza <pedro.henrique@defensoria.mg.def.br>
2024-09-19 10:01:36 -0300

    wip: password generation structure

---
+++ src/services/password/create.js
@@ -0,0 +1,8 @@
+import chalk from "chalk"
+
+function createPassword() {
+  console.log(chalk.green("Password"));
+
+}
+
+export { createPassword }
---
+++ src/services/password/handle.js
@@ -0,0 +1,6 @@
+function handle(params) {
+  let characters = []
+  let password = ""
+}
+
+export {handle}
